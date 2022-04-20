5cc13f01dbf0f70be74a2b0c4d2b95c386c9341e
Macarena Ruiz <contactomruiz@yahoo.com>
2022-04-20 16:11:58 -0300

    timer fix

--- Runner Motores/Assets/Scripts/PlayerMovement.cs
+++ Runner Motores/Assets/Scripts/PlayerMovement.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public int JumpForce;
     public int speed = 7;
     public Rigidbody rb;
+    public Text timerText;
+    float startTime;
+    bool meta = false;
     
     
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
 
     void Update()
     {
+        if (meta)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
         if (Input.GetKeyDown("space"))
         {
             this.GetComponent<Rigidbody>().AddForce(new Vector3(0, JumpForce, 0));
@@ -36,5 +49,11 @@ public class PlayerMovement : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+    }
+
+    public void Meta()
+    {
+        meta = true;
+        timerText.color = Color.green;
     }
 }
